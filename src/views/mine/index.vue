ed4e0a23bc4b0ec67487a9fbc721933d8f63f98f
itcast <itcast@itcast.cn>
2020-03-24 13:34:24 +0800

    miaomiao-first

---
+++ src/views/mine/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+       <Header title="我的喵喵"></Header>
+       <login></login>
+       <tabBar></tabBar>
+    </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import tabBar from '@/components/tabBar'
+import login from '@/components/login'
+export default {
+    name: 'mine',
+        components:{
+        Header,
+        tabBar,
+        login
+    }
+}
+</script>
+<style>
+
+</style>
